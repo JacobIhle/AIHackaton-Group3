30d59d6addece6bfd5f115a0a48ceda68a38bad8
Jacob Ihle <Jacob.Vetle.Kvinnsland.Ihle@akerbp.com>
2023-09-23 01:37:59 +0200

    added speech to text api

---
+++ API/ConsoleFileToText/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech;
+
+internal class Program
+{
+    static string speechKey = Environment.GetEnvironmentVariable("SpeechKey");
+    static string speechRegion = Environment.GetEnvironmentVariable("SpeechRegion");
+
+    private static async Task Main(string[] args)
+    {
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+        speechConfig.SpeechRecognitionLanguage = "en-US";
+
+        // Selects the audio input to be a file
+        using var audioConfig = AudioConfig.FromWavFileInput("fruits.wav");
+        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        Console.WriteLine("Listening to file");
+        await SpeechToTextCL.SpeechServices.RecognizeSpeech(speechRecognizer);
+    }
+}
---
+++ API/SpeechToTextCL/SpeechServices.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechToTextCL
+{
+    public static  class SpeechServices
+    {
+        public static async Task<string> RecognizeSpeech(SpeechRecognizer speechRecognizer)
+        {
+            var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+            return OutputSpeechRecognitionResult(speechRecognitionResult);
+        }
+        public static async Task RecognizeOnceAsync(SpeechRecognizer speechRecognizer)
+        {
+            Console.WriteLine("Listening to file");
+            var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+            OutputSpeechRecognitionResult(speechRecognitionResult);
+        }
+
+        static string OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
+        {
+            switch (speechRecognitionResult.Reason)
+            {
+                case ResultReason.RecognizedSpeech:
+                    Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                    return speechRecognitionResult.Text;
+
+                case ResultReason.NoMatch:
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                    }
+                    break;
+            }
+            return "Speech could not be recognized";
+        }
+    }
+}
